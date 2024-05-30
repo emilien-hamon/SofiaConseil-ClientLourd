3d50f3651d7a948a2b698ff9858c64f92f5c793c
Emilien <emilienhamon@outlook.com>
2024-05-30 11:46:03 +0200

    Ajout 2 page : Competence, MainWindow

---
+++ SofiaConseil.Wpf/ViewModel/ViewModelMainWindow.cs
@@ -0,0 +1,24 @@
+﻿using SofiaConseil.DbLib.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofiaConseil.Wpf.ViewModel
+{
+    internal class ViewModelMainWindow : ViewModelBase
+    {
+        public string Email { get; set; }
+
+
+        public ViewModelMainWindow()
+        {
+            using (SofiaConseilContext context = new())
+            {
+                Email = context.Users.First().Email;
+            }
+        }
+
+    }
+}
---
+++ SofiaConseil.Wpf/Views/ViewCompetences.xaml.cs
@@ -0,0 +1,35 @@
+﻿using SofiaConseil.Wpf.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SofiaConseil.Wpf.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour ViewCompetences.xaml
+    /// </summary>
+    public partial class ViewCompetences : UserControl
+    {
+        public ViewCompetences()
+        {
+            this.DataContext = new ViewModelCompetences();
+            InitializeComponent();
+        }
+
+        private void ButtonAddCompetence_Click(object sender, RoutedEventArgs e)
+        {
+            ((ViewModelCompetences)this.DataContext).AddCompetence();
+        }
+    }
+}
